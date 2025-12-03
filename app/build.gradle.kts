a5bf0496b9efa98d5624448d08c24acf60f5c609
A <666av6@gmail.com>
2025-12-04 00:00:57 +0300

    add publishing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "ru.vafeen.directrefresher"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ru.vafeen.directrefresher"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
--- direct-refresher/build.gradle.kts
+++ direct-refresher/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    `maven-publish`
+    alias(libs.plugins.com.vanniktech.maven.publish)
 }
+
+group = project.findProperty("group").toString()
+version = project.findProperty("version").toString()
 
 android {
     namespace = "io.github.vafeen.direct_refresher"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
@@ -44,16 +47,39 @@ dependencies {
     api(libs.retrofit)
     api(libs.converter.gson)
 }
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "android.vafeen"
-            artifactId = "direct_refresher"
-            version = "1.0.3"
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
 
-            afterEvaluate {
-                from(components.getByName("release"))
+    coordinates(
+        group as String?,
+        "direct-refresher",
+        version as String?
+    )
+
+    pom {
+        name.set("Direct Refresher library")
+        description.set("Direct Refresher library")
+        inceptionYear.set("2025")
+        url.set("https://github.com/vafeen/direct-refresher")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
+        }
+        developers {
+            developer {
+                id.set("vafeen")
+                name.set("A")
+                url.set("https://github.com/vafeen/")
+            }
+        }
+        scm {
+            url.set("https://github.com/vafeen/direct-refresher")
+            connection.set("scm:git:git://github.com/vafeen/direct-refresher.git")
+            developerConnection.set("scm:git:ssh://git@github.com/vafeen/direct-refresher.git")
         }
     }
 }
